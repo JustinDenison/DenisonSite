d57d0ed569c4ba3bdf1be515bd8e3858a82bd1ec
justin <justin@justin.justin>
2026-02-03 17:58:50 -0600

    chore: organize pages into individual folders; remove top-level duplicates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Certifications } from './certifications/certifications.component';
+import { About } from './about/about.component';
+import { Projects } from './projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
+  { path: 'certifications', component: Certifications },
+  { path: 'about', component: About },
+  { path: 'projects', component: Projects },
+];
---
+++ src/app/certifications/certifications.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certifications',
+  standalone: true,
+  templateUrl: './certifications.component.html',
+  styleUrls: ['./certifications.component.scss']
+})
+export class Certifications {}
