99864acbe883882a14ac544e002ad3654b1aacd0
justin <justin@justin.justin>
2026-02-03 18:02:39 -0600

    feat: add Home page and route; add Home nav link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home.component';
 import { Certifications } from './certifications/certifications.component';
 import { About } from './about/about.component';
 import { Projects } from './projects/projects.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'about', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
   { path: 'certifications', component: Certifications },
   { path: 'about', component: About },
   { path: 'projects', component: Projects },
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,4 +1,5 @@
 export * from './main/app';
+export * from './home/home.component';
 export * from './about/about.component';
 export * from './certifications/certifications.component';
 export * from './projects/projects.component';
