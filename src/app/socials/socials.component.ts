a23b55e6efd981d1df07a04897e3eb12324cb023
justin <justin@justin.justin>
2026-02-03 18:14:49 -0600

    feat(socials): editable social links UI with localStorage persistence and spaced buttons

--- src/app/socials/socials.component.ts
+++ src/app/socials/socials.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-socials',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './socials.component.html',
   styleUrls: ['./socials.component.scss']
 })
-export class Socials {}
+export class Socials implements OnInit {
+  facebook = '';
+  instagram = '';
+  linkedin = '';
+  github = '';
+
+  ngOnInit() {
+    this.facebook = localStorage.getItem('social:facebook') ?? '';
+    this.instagram = localStorage.getItem('social:instagram') ?? '';
+    this.linkedin = localStorage.getItem('social:linkedin') ?? '';
+    this.github = localStorage.getItem('social:github') ?? '';
+  }
+
+  save() {
+    localStorage.setItem('social:facebook', this.facebook);
+    localStorage.setItem('social:instagram', this.instagram);
+    localStorage.setItem('social:linkedin', this.linkedin);
+    localStorage.setItem('social:github', this.github);
+  }
+
+  clear() {
+    this.facebook = '';
+    this.instagram = '';
+    this.linkedin = '';
+    this.github = '';
+    this.save();
+  }
+}
